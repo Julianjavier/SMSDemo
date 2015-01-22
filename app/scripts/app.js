ff1da996cc83f22364299cb3034a77f0bda1b86a
Julian Rodriguez <greyfox893@mail.com>
2015-01-22 18:57:42 -0500

    Initialize Project

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,39 +1,17 @@
-var demoApp = angular.module('demoApp', ["timer", "ngRoute"]);
+var timeSyncApp = angular.module('timeSyncApp', ["timer", "ngRoute", "firebase"]);
 
-demoApp.config(['$routeProvider', function($routeProvider){
+timeSyncApp.config(['$routeProvider', function($routeProvider){
 	
 	$routeProvider.when('/', {
 		
-		templateUrl:'form.html',
-		controller:'demoInputs'
+		templateUrl:'view/form.html',
+		controller:'timeInputs'
 	
-	}).when('/demoTime', {
+	}).when('/getTime/:id', {
 		
-		templateUrl:'timer.html',
-		controller:'demoTime'
+		templateUrl:'view/timer.html',
+		controller:'getTime',
 	
 	});
 
 }]);
-
-///////
-demoApp.controller('demoInputs', ['$scope','$rootScope','$location' ,function($scope, $rootScope, $location){
-	$scope.submit = function() {
-        if ($scope.hours || $scope.minutes || $scope.seconds) {
-          var hoursInSeconds = this.hours * 3600;
-          var minutesInSeconds = this.minutes * 60;
-          var seconds = $scope.seconds;
-
-          var fullTime = hoursInSeconds + minutesInSeconds + seconds;
-          $rootScope.fullTime = fullTime;
-          fullTime = 0;
-          $location.path('/demoTime');
-    	}
-    };
-}]);
-
-demoApp.controller('demoTime', ['$scope','$rootScope' ,function($scope, $rootScope){
-	console.log($rootScope.fullTime);
-
-}]);
-
